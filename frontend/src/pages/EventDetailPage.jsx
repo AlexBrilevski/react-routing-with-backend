38d91d3fd18a3d09702da05230724ba85bb4184d
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-23 16:18:07 +0300

    Event listing and retrieving URL params on the event details page was added

--- frontend/src/pages/EventDetailPage.jsx
+++ frontend/src/pages/EventDetailPage.jsx
@@ -1,6 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const EventDetailPage = () => {
+  const params = useParams();
+
   return (
-    <h1>The Event Detail Page</h1>
+    <>
+      <h1>The Event Detail Page</h1>
+      <p>{params.eventId}</p>
+    </>
   );
 };
 
--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
+
+const EVENTS_DATA = [
+  { id: 'e1', title: 'Event 1' },
+  { id: 'e2', title: 'Event 2' },
+  { id: 'e3', title: 'Event 3' },
+  { id: 'e4', title: 'Event 4' },
+];
+
 const EventsPage = () => {
   return (
-    <h1>The Events Page</h1>
+    <>
+      <h1>The Events Page</h1>
+      <ul>
+        {EVENTS_DATA.map(event => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
