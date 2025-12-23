cd53b54386027a6cef2636b67fc6ce70579ec1d8
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-23 15:50:47 +0300

    Root layout and basic page routing were added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RootLayout from './RootLayout';
+import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import NewEventPage from './pages/NewEventPage';
+import EditEventPage from './pages/EditEventPage';
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: 'events', element: <EventsPage /> },
+        { path: 'events/:eventId', element: <EventDetailPage /> },
+        { path: 'events/new', element: <NewEventPage /> },
+        { path: 'events/:eventId/edit', element: <EditEventPage /> },
+      ]
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
