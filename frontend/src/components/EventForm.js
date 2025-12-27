fa1b8d131bde1a39f5ea455fb5fdfad9ad2de585
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-27 13:06:55 +0300

    Updating the event form UI state based on submission was implemented

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,12 @@
-import { useNavigate, Form } from 'react-router-dom';
+import { useNavigate, useNavigation, Form } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   function cancelHandler() {
     navigate('..');
   }
@@ -47,10 +50,12 @@ function EventForm({ method, event }) {
           defaultValue={event ? event.description : ''} />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'Save'}
+        </button>
       </div>
     </Form>
   );
